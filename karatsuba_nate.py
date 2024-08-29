eb5567bb58911d14f5e5cb2eca9d1a56dd34870f
Nathan Hanzel <nathan.hanzel@gmail.com>
2024-08-28 18:38:53 -0600

    Added lookup table and adjusted for a base case of one digit

--- karatsuba_nate.py
+++ karatsuba_nate.py
@@ -7,17 +7,18 @@
 from helper import *
 
 def karatsuba(x, y):
+    x,y = fix_lengths(x,y)
+    print("x: " + x + " y: " + y)
     n = len(x)
-    a , b = split_string_number(x)
-    c, d = split_string_number(y)
-    print("split size: " + str(n))
-    if (len(a) > 2): # 2 digits is our base case (a, b, c, and d will always have the same number of digits)
+    if (n == 1): # 1 digit is our base case (a, b, c, and d will always have the same number of digits)
+        return mult_lookup_table(x + y)
+    else:
+        a , b = split_string_number(x)
+        c, d = split_string_number(y)
         ac = int(karatsuba(a, c))
         bd = int(karatsuba(b, d))
         ad_bc = int(karatsuba(str(int(a)+int(b)), str(int(c)+int(d)))) - ac - bd
         return str((ac * 10 ** n) + bd + (ad_bc * 10 ** (n // 2)))
-    else:
-        return str(int(x) * int(y))
 
 x = input("Enter first number: ")
 y = input("Enter second number: ")
